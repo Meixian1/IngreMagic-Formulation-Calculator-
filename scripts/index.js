f08cd095156bb7b7b93130f5abea8c04983dbc40
Applebees1 <104962202+Applebees1@users.noreply.github.com>
2023-08-31 18:56:57 -0700

    Week5Project1

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+
+
+// Form input elements in Container 3
+const percentageInput = document.querySelector('.container3 input[name="Percentage"]');
+const ozInput = document.querySelector('.container3 input[name="oz"]');
+const priceInput = document.querySelector('.container3 input[name="Price"]');
+
+
+
+//For Ingredient Functions, use Arrays
+//Function for the Submit Button in the batchContainer area
+//Function for the Add Button in the container1 area 
+//FUnction for the Edit Button
+//Function for the Display Area in the container2 area 
+//Function for the Calculating Sum Button 
+//Calculating for percentage %, with the known total amount to be made, ex. 
+//Calculating for the oz
+//calculating for the price 
+//How does input in container1 will be displayed in container2 
+//How does displays in contianer2 will be claculated in container3?
+//How does the checkBox connect to the Edit Button? 
+//Making sure that the checkBoxs are in fixed position. 
+//Making sure that all the buttons are in fixed position. 
+//Overall checking whether or not all parts are communicating and functioning as desired!!!
